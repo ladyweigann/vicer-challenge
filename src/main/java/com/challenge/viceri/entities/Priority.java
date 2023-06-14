512964dca483277f83e90a6be08e5042b012e69b
Larissa Labanca <ext.lcpontes@unicid.edu.br>
2023-06-14 13:18:59 -0300

    refactoring and add a new method to receive a integer from the db and change it to the class type

--- src/main/java/com/challenge/viceri/entities/Priority.java
+++ src/main/java/com/challenge/viceri/entities/Priority.java
@@ -1,6 +1,7 @@
 package com.challenge.viceri.entities;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public enum Priority {
     LOW(1, "Baixa"),
@@ -23,11 +24,28 @@ public enum Priority {
         return description;
     }
 
-    public Integer getPriority(String priority){
+    public static Integer getPriority(String priority){
         return Arrays.stream(Priority.values())
                 .filter(p -> p.getDescription().equalsIgnoreCase(priority))
                 .map(Priority::getValue)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("Invalid priority"));
+    }
+
+    public static String getPriority(Integer priority) {
+        return Arrays.stream(Priority.values())
+                .filter(p -> Objects.equals(p.getValue(), priority))
+                .map(Priority::getDescription)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Invalid priority"));
+    }
+
+    public static Priority fromValue(Integer value) {
+        for (Priority priority : values()) {
+            if (Objects.equals(priority.getValue(), value)) {
+                return priority;
+            }
+        }
+        throw new IllegalArgumentException("Invalid priority value: " + value);
     }
 }
--- src/main/java/com/challenge/viceri/entities/Status.java
+++ src/main/java/com/challenge/viceri/entities/Status.java
@@ -1,6 +1,7 @@
 package com.challenge.viceri.entities;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public enum Status {
     PENDING(1, "Pendente"),
@@ -22,11 +23,28 @@ public enum Status {
         return description;
     }
 
-    public Integer getPriority(String status){
+    public static Integer getStatus(String status){
         return Arrays.stream(Status.values())
                 .filter(p -> p.getDescription().equalsIgnoreCase(status))
                 .map(Status::getValue)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("Invalid status"));
+    }
+
+    public static String getStatus(Integer status) {
+        return Arrays.stream(Status.values())
+                .filter(p -> Objects.equals(p.getValue(), status))
+                .map(Status::getDescription)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Invalid status"));
+    }
+
+    public static Status fromValue(Integer value) {
+        for (Status status : values()) {
+            if (Objects.equals(status.getValue(), value)) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("Invalid status value: " + value);
     }
 }
